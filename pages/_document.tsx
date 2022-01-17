4666cb5482168a530ddb9532438cc6a76cd56787
Eldes <studio@eldes.com>
2022-01-17 13:59:38 -0300

    [=] Fix favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,29 +7,35 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<meta charSet="utf-8" />
-					<meta name="application-name" content="Eldes" />
-					<meta name="theme-color" content="#ffffff"></meta>
-					<link rel="shortcut icon" href="/icons/favicon.ico"/>
-					<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-					<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-					<link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
-					<meta name="apple-mobile-web-app-title" content="Eldes" />
-					<meta name="apple-mobile-web-app-capable" content="yes" />
-					<link rel="apple-touch-startup-image" href="/icons/apple-touch-launch.png" />
-					<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-					<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
-					<link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
-					<link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png" />
-					<link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png" />
-					<link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png" />
-					<link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
-					<link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png" />
-					<link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
-					<link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png" />
-					<link rel="manifest" href="/icons/site.webmanifest" />
-					<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#ffffff" />
-					<meta name="msapplication-TileColor" content="#ffffff" />
-					<meta name="msapplication-config" content="/icons/browserconfig.xml" />
+					{/* Chrome for Android: */}
+					<link rel="icon" sizes="512x512" href="/public/icons/android-chrome-512x512.png" />
+					<link rel="icon" sizes="192x192" href="/public/icons/android-chrome-192x192.png" />
+					{/* iPhone 6 Plus with @3x display: */}
+					<link rel="apple-touch-icon-precomposed" sizes="180x180" href="/public/icons/apple-touch-icon-180x180-precomposed.png" />
+					{/* iPad with @2x display running iOS 7 or later: */}
+					<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/public/icons/apple-touch-icon-152x152-precomposed.png" />
+					{/* iPad with @2x display running iOS 6 or earlier: */}
+					<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/public/icons/apple-touch-icon-144x144-precomposed.png" />
+					{/* iPhone with @2x display running iOS 7 or later: */}
+					<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/public/icons/apple-touch-icon-120x120-precomposed.png" />
+ 					{/* iPhone with @2x display running iOS 6 or earlier: */}
+					<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/public/icons/apple-touch-icon-114x114-precomposed.png" />
+					{/* iPad mini and the first- and second-generation iPad (with @1x display) running iOS 7 or later: */}
+					<link rel="apple-touch-icon-precomposed" sizes="76x76" href="/public/icons/apple-touch-icon-76x76-precomposed.png" />
+ 					{/* iPad mini and the first- and second-generation iPad (with @1x display) running iOS 6 or earlier: */}
+					<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/public/icons/apple-touch-icon-72x72-precomposed.png" />
+ 					{/* non-Retina iPhone, iPod Touch, and Android 2.1+ devices: */}
+					<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/public/icons/apple-touch-icon-57x57-precomposed.png" />
+					{/* basic favicon */}
+					<link rel="icon" href="/icons/favicon.svg" type="image/svg+xml"/>
+					<link rel="icon" sizes="32x32" href="/icons/favicon-32x32.png" type="image/png" />
+					<link rel="icon" sizes="16x16" href="/icons/favicon-16x16.png" type="image/png" />
+					{/* default favicon */}
+					<link rel="icon" href="favicon.ico" type="image/x-icon" />
+					{/* theme color: */}
+					<meta name="theme-color" media="(prefers-color-scheme: light)" content="#000" />
+					<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+					{/* fonts: */}
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400&display=swap" rel="stylesheet" />
 				</Head>
