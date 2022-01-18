575cf0c47491e0d8d2bc85a80ebe4588c0bdff48
Eldes <studio@eldes.com>
2022-01-18 10:32:58 -0300

    [!] Fix link

--- pages/childrens-books-and-textbooks.tsx
+++ pages/childrens-books-and-textbooks.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 import Page, { PageSection } from '../components/Page'
 
 const ChildrensBooksAndTextbooksPage: NextPage = () => {
 	return (
 		<Page title="Children's Books and Textbooks" section={PageSection.Portfolio}>
-			<a href="/brasil-na-bagagem-textbook-illustrations">Brasil na Bagagem textbook illustrations</a>
+			<Link href="/brasil-na-bagagem-textbook-illustrations"><a >Brasil na Bagagem textbook illustrations</a></Link>
 		</Page>
 	)
 }
