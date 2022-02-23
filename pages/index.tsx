c55adf642e533093e2f887188ba7bf2bf9e7effc
Eldes <studio@eldes.com>
2022-02-23 09:39:04 -0300

    [=] refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Page, { PageSection } from '../components/Page'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-return (
-	<Page title="Homepage" section={ PageSection.Home } />
-)
+	return (
+		<Page title="Homepage" section={ PageSection.Home } />
+	)
 }
 
 export default Home
