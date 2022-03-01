d6618fec976e9e0d119f9e953ba2345f79da79c2
Eldes <studio@eldes.com>
2022-03-01 17:32:25 -0300

    [+] Add Fonts page basic

---
+++ pages/fonts.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+import Page, { PageSection } from '../components/Page'
+
+const FontsPage: NextPage = () => {
+	return (
+		<Page title="Fonts" section={PageSection.Fonts}>
+		</Page>
+	)
+}
+
+export default FontsPage
