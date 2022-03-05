6b6b86282a05e0174a48ddabc7cf60af334d03e4
Eldes <studio@eldes.com>
2022-03-05 16:37:27 -0300

    [+] Add notes field to BeCredits component

--- components/BeCredits.tsx
+++ components/BeCredits.tsx
@@ -7,12 +7,15 @@ type Props = {
 	work?: string
 }
 
-const BeCredits: FunctionComponent<Props> = ({ clientName, work }) => {
+const BeCredits: FunctionComponent<Props> = ({ clientName, work, children }) => {
 	return (
 		<BeComponent>
 			<p className={ styles.beCredits }>
 				<span className={ styles.label }>Client:</span> { clientName }<br/>
 				<span className={ styles.label }>Work:</span> { work }
+				{
+					children && <span className={ styles.notes }>{ children }</span>
+				}
 			</p>
 		</BeComponent>
 	)
