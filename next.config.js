dedcd233908d239d92f793fdc4d51edfd0034993
Eldes <studio@eldes.com>
2022-05-11 14:55:02 -0300

    [+] Add redirect from Home to Portfolio

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
 	reactStrictMode: true,
 	i18n,
+
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/portfolio',
+				permanent: false,
+			}
+		]
+	},
 }
