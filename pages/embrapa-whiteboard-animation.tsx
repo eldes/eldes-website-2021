68fe3449207dc96663d4ac39b22688d9f2563cd7
Eldes <studio@eldes.com>
2022-05-12 17:47:16 -0300

    [+] Add portfolio piece "Embrapa whiteboard animation"

--- pages/embrapa-whiteboard-animation.tsx
+++ pages/embrapa-whiteboard-animation.tsx
@@ -1,13 +1,22 @@
-import { GetStaticProps, NextPage } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import BeCredits from '../components/BeCredits'
-import PieceBePage, { pieceBePageI18nNamespace } from '../components/PieceBePage'
+import { GetStaticProps, NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import BeCredits from '../components/BeCredits';
+import BeEmbed from '../components/BeEmbed';
+import BeImage from '../components/BeImage';
+import BeThanks from '../components/BeThanks';
+import PieceBePage, { pieceBePageI18nNamespace } from '../components/PieceBePage';
+import highlightImage from '../public/content/embrapa-whiteboard-animation/highlight.jpg';
 
 const EmbrapaWhiteboardAnimationPage: NextPage = () => {
 
 	return (
 		<PieceBePage>
+			<BeImage src={highlightImage} alt={'Details of illustation'} />
 			<BeCredits clientName='Embrapa' work='illustration' />
+			<BeEmbed aspectRatioPercent={56.25} horizontalPadding={true} verticalPadding={true}>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/LnnnHaToJJE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+			</BeEmbed>
+			<BeThanks />
 		</PieceBePage>
 	)
 }
