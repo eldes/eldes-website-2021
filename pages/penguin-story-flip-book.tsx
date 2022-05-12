91c5527ecb3af256722ac7092e52bba67ecf5c34
Eldes <studio@eldes.com>
2022-05-12 17:53:46 -0300

    [+] Add portfolio piece "Penguin Story flip book"

--- pages/penguin-story-flip-book.tsx
+++ pages/penguin-story-flip-book.tsx
@@ -1,12 +1,18 @@
-import { GetStaticProps, NextPage } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import BeCredits from '../components/BeCredits'
-import PieceBePage, { pieceBePageI18nNamespace } from '../components/PieceBePage'
+import { GetStaticProps, NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import BeCredits from '../components/BeCredits';
+import BeEmbed from '../components/BeEmbed';
+import BeThanks from '../components/BeThanks';
+import PieceBePage, { pieceBePageI18nNamespace } from '../components/PieceBePage';
 
 const PenguinStoryFlipBookPage: NextPage = () => {
 	return (
 		<PieceBePage>
 			<BeCredits clientName='private' work='illustration' />
+			<BeEmbed aspectRatioPercent={56.25} horizontalPadding={true} verticalPadding={true}>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/bFmu-1aW7qc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+			</BeEmbed>
+			<BeThanks />
 		</PieceBePage>
 	)
 }
