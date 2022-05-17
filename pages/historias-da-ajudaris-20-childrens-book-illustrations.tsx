4c166da5c4bab13d1320fd118977fc4de9d4dcdd
Eldes <studio@eldes.com>
2022-05-17 19:18:02 -0300

    [=] Adjust Lint issues

--- pages/historias-da-ajudaris-20-childrens-book-illustrations.tsx
+++ pages/historias-da-ajudaris-20-childrens-book-illustrations.tsx
@@ -1,16 +1,16 @@
-import { GetStaticProps, NextPage } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import BeCredits from '../components/BeCredits'
-import BeImage from '../components/BeImage'
-import BeParagaph, { BeParagaphAlign } from '../components/BeParagaph'
-import BePhotoGrid from '../components/BePhotoGrid'
-import BeThanks from '../components/BeThanks'
-import PieceBePage, { pieceBePageI18nNamespace } from '../components/PieceBePage'
-import coverImage from '../public/content/ajudaris-20-childrens-book-illustrations/cover.jpg'
-import giraffeImage from '../public/content/ajudaris-20-childrens-book-illustrations/giraffe.jpg'
-import animaisCacadorImage from '../public/content/ajudaris-20-childrens-book-illustrations/os-animais-e-o-cacador.jpg'
-import roughImage from '../public/content/ajudaris-20-childrens-book-illustrations/rough.jpg'
-import forestImage from '../public/content/ajudaris-20-childrens-book-illustrations/the-forest.jpg'
+import { GetStaticProps, NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import BeCredits from '../components/BeCredits';
+import BeImage from '../components/BeImage';
+import BeParagaph, { BeParagaphAlign } from '../components/BeParagaph';
+import BePhotoGrid from '../components/BePhotoGrid';
+import BeThanks from '../components/BeThanks';
+import PieceBePage, { pieceBePageI18nNamespace } from '../components/PieceBePage';
+import coverImage from '../public/content/ajudaris-20-childrens-book-illustrations/cover.jpg';
+import giraffeImage from '../public/content/ajudaris-20-childrens-book-illustrations/giraffe.jpg';
+import animaisCacadorImage from '../public/content/ajudaris-20-childrens-book-illustrations/os-animais-e-o-cacador.jpg';
+import roughImage from '../public/content/ajudaris-20-childrens-book-illustrations/rough.jpg';
+import forestImage from '../public/content/ajudaris-20-childrens-book-illustrations/the-forest.jpg';
 
 
 const HistoriasDaAjudaris20ChildrensBookIllustrationsPage: NextPage = () => {
