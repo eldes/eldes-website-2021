93b206228da64a4d6c8c5eb4a8f64438e95ef742
Eldes <studio@eldes.com>
2022-06-12 18:56:49 -0300

    [!] Redirect eldes.com.br to eldes.com/br

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,15 @@ module.exports = {
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'br'],
+		domains: [
+			{
+				domain: 'eldes.com',
+				defaultLocale: 'en',
+			},
+			{
+				domain: 'eldes.com.br',
+				defaultLocale: 'br',
+			},
+		]
 	},
 }
