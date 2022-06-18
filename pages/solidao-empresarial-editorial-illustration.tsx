f02a4049bc0116adf6061399f46993c5c73ad6cf
Eldes <studio@eldes.com>
2022-06-18 19:58:59 -0300

    [+] Add portfolio piece "Solidão Empresarial"

---
+++ pages/solidao-empresarial-editorial-illustration.tsx
@@ -0,0 +1,77 @@
+import { NextPage } from 'next';
+import BeCredits from '../components/BeCredits';
+import BeImage from '../components/BeImage';
+import BeParagaph from '../components/BeParagaph';
+import BePhotoGrid from '../components/BePhotoGrid';
+import BeThanks from '../components/BeThanks';
+import PieceBePage from '../components/PieceBePage';
+import SeeMore from '../models/SeeMore';
+import illustrationImage from '../public/content/solidao-empresarial-editorial-illustration/illustration.png';
+import mockupImage from '../public/content/solidao-empresarial-editorial-illustration/mockup.jpg';
+import sketchbook1Image from '../public/content/solidao-empresarial-editorial-illustration/sketchbook-1.jpg';
+import sketchbook2Image from '../public/content/solidao-empresarial-editorial-illustration/sketchbook-2.jpg';
+
+const SolidaoEmpresarialPage: NextPage = () => {
+  const seeMoreList: SeeMore[] = [
+		{
+			title: {
+				en: 'More for editorial',
+				br: 'Mais para editorial',
+			},
+			slugs: [
+        'hipocrisil',
+      ],
+		},
+		{
+			title: {
+				en: 'More in woodcut style',
+				br: 'Mais no estilo xilo',
+			},
+			slugs: [
+        'como-eram-os-animais-cordel-childrens-book',
+			],
+		},
+	];
+  
+  return (
+    <PieceBePage seeMoreList={seeMoreList}>
+      <BeImage src={illustrationImage} alt={'Illustration'} />
+      <BeCredits fields={[
+        {
+          label: 'Luciano Pires',
+          value: {
+            br: 'texto',
+            en: 'text'
+          },
+        },
+        {
+          label: 'Eldes',
+          value: {
+            br: 'ilustração',
+            en: 'illustration'
+          },
+        },
+      ]} />
+      <BeParagaph>
+        O artigo &quot;Solidão empresarial&quot; foi escrito pelo escritor Luciano Pires e publicado na seção de artigos semanais do seu blog &quot;Café Brasil&quot;.
+      </BeParagaph>
+      <BeImage src={mockupImage} alt="Mock-up" />
+      <BeParagaph>
+        Ilustrar alguns desses artigos semanais, durante dois anos, foi importante para definir meu método de desenvolvimento de ilustrações para periódicos — quando se conta com um tempo relativamente curto entre receber o texto e elaborar a ilustração.
+      </BeParagaph>
+      <BePhotoGrid images={[
+        {
+          src: sketchbook1Image,
+          alt: 'Sketch',
+        },
+        {
+          src: sketchbook2Image,
+          alt: 'Sketch',
+        },
+      ]} cols={2} />
+      <BeThanks />
+    </PieceBePage>
+  );
+};
+
+export default SolidaoEmpresarialPage;
