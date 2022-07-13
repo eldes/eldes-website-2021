c9e8768b2fe5f1e12335687acb4bd6a68e24e4da
Eldes <studio@eldes.com>
2022-07-12 21:02:01 -0300

    [+] property link to BeImage

--- components/BeImage.tsx
+++ components/BeImage.tsx
@@ -5,33 +5,35 @@ import styles from '../styles/BeImage.module.scss';
 import { BeImageData } from './BeBody';
 import BeComponent, { beComponentDefaultProps, BeComponentProps } from './BeComponent';
 
-type Props = BeComponentProps & BeImageData
+type Props = BeComponentProps & BeImageData & {
+	link?: string
+}
 
-const BeImage: FunctionComponent<Props> = ({ fill, foregroundColor, backgroundColor, horizontalPadding, verticalPadding, src, width, height, alt, round }) => {
-	const className = round ? `${styles.beImage} ${styles.round}` : styles.beImage
+const BeImage: FunctionComponent<Props> = (props) => {
+	const className = props.round ? `${styles.beImage} ${styles.round}` : styles.beImage
 
 	return (
 		<BeComponent
 			subClassName={ className }
-			fill={ fill }
-			foregroundColor={ foregroundColor }
-			backgroundColor={ backgroundColor }
-			horizontalPadding={ horizontalPadding }
-			verticalPadding={ verticalPadding }
+			fill={ props.fill }
+			foregroundColor={ props.foregroundColor }
+			backgroundColor={ props.backgroundColor }
+			horizontalPadding={ props.horizontalPadding }
+			verticalPadding={ props.verticalPadding }
 		>
-			<Link href={src.src}>
+			<Link href={props.link ? props.link : props.src.src}>
 			{
-				fill ?
+				props.fill ?
 				<Image
-					src={ src }
-					alt={ alt }
+					src={ props.src }
+					alt={ props.alt }
 					layout="responsive"
 				/> :
 				<Image
-					src={ src }
-					alt={ alt }
-					width={ width }
-					height={ height }
+					src={ props.src }
+					alt={ props.alt }
+					width={ props.width }
+					height={ props.height }
 					layout="intrinsic"
 				/>
 			}
