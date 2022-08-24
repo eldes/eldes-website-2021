b12376cc63a3813a10fd8bf91c6fe5be0136f0b6
Eldes <studio@eldes.com>
2022-08-24 15:31:37 -0300

    [+] Piece tags

---
+++ lib/Helpers.ts
@@ -0,0 +1,74 @@
+import { NextRouter } from 'next/router';
+import SeeMore from '../models/SeeMore';
+import Tag, { TagSlug } from '../models/Tag';
+import categoriesRepository from '../repositories/categories-repository';
+import piecesRepository from '../repositories/pieces-repository';
+import TagsRepository from '../repositories/tags-repository';
+
+const getCurrentPageSlug: (router: NextRouter) => string = (router: NextRouter) => router.pathname.substring(1).split('#')[0];
+
+const rotateArrayToLeftUntilToBeFirst: <T>(array: T[] | undefined, willBeFirst: T) => T[] | undefined = <T>(array: T[] | undefined, willBeFirst: T) => {
+
+  if ((array !== undefined) && (array.length > 0) && (array.indexOf(willBeFirst) !== -1) && (array[0] !== willBeFirst)) {
+    const newLast = array[0];
+    array.shift();
+    if (newLast) {
+      array.push(newLast);
+      return rotateArrayToLeftUntilToBeFirst<T>(array, willBeFirst);
+    }
+  }
+   
+  return array;
+};
+
+const makeSeeMoreListForPieceBePage: (router: NextRouter, tagSlugs: TagSlug[]) => SeeMore[] = (router: NextRouter, tagSlugs: TagSlug[]) => {
+  const seeMoreList: SeeMore[] = [];
+
+  const currentPieceSlug = Helpers.getCurrentPageSlug(router);
+  const categories = piecesRepository.loadAllCategories(currentPieceSlug);
+
+  categories.forEach(category => {
+    const allPieceSlugsWithCategory = category.pieceSlugs;
+    const rotatedPieceSlugsWithCategory = Helpers.rotateArrayToLeftUntilToBeFirst<string>(allPieceSlugsWithCategory, currentPieceSlug);
+    const otherPiecesSlugsWithCategory = rotatedPieceSlugsWithCategory?.filter(pieceSlug => ((pieceSlug !== currentPieceSlug) && (seeMoreList.map(seeMore => seeMore.slugs).flat().indexOf(pieceSlug) === -1)));
+
+    if (otherPiecesSlugsWithCategory?.length) {
+      seeMoreList.push({
+        title: {
+          en: `More ${category?.title.en}`,
+          br: `Mais ${category?.title.br}`,
+        },
+        slugs: [otherPiecesSlugsWithCategory[0]],
+      });
+    }
+  });
+
+  tagSlugs.forEach(tagSlug => {
+    const tag = TagsRepository.get(tagSlug);
+    const allPieceSlugsWithTag = tag?.pieceSlugs;
+    const rotatedPieceSlugsWithTag = Helpers.rotateArrayToLeftUntilToBeFirst<string>(allPieceSlugsWithTag, currentPieceSlug);
+    const otherPieceSlugsWithTag = rotatedPieceSlugsWithTag?.filter(pieceSlug => ((pieceSlug !== currentPieceSlug) && (seeMoreList.map(seeMore => seeMore.slugs).flat().indexOf(pieceSlug) === -1)));
+
+    const titlePrefix = (tag?.pieceSlugs.indexOf(currentPieceSlug) !== -1) ? {en: 'More', br: 'Mais'} : {en: 'See also', br: 'Veja também'}
+
+    if (otherPieceSlugsWithTag?.length) {
+      seeMoreList.push({
+        title: {
+          en: `${titlePrefix.en} ${tag?.title.en}`,
+          br: `${titlePrefix.br} ${tag?.title.br}`,
+        },
+        slugs: [otherPieceSlugsWithTag[0]],
+      });
+    }
+  });
+
+  return seeMoreList;
+};
+
+const Helpers = {
+  getCurrentPageSlug,
+  rotateArrayToLeftUntilToBeFirst,
+  makeSeeMoreListForPieceBePage,
+};
+
+export default Helpers;
