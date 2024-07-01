1a1d14a782372fa9c44cb3145bfd3fbbe65254ee
Eldes <github@eldes.com>
2024-06-30 23:45:18 -0300

    [+] Generate og fields at font pages

--- models/Font.ts
+++ models/Font.ts
@@ -1,7 +1,10 @@
+import Localized from './Locale';
+
 type Font = {
-	slug: string
-	name: string
-	thumbnail: string
+	slug: string;
+	name: string;
+	thumbnail: string;
+	description: Localized<string>;
 }
 
 export default Font
